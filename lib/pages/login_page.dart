3b6a3860fc7c349d7d7eaaae53ce9c1357c9b27a
Hashir Saud Khan <hashirsaudkhan@gmail.com>
2024-05-05 13:47:24 +0500

    Authenticate google sign in with firebase

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/widgets.dart';
 import 'package:modernlogintute/components/my_button.dart';
 import 'package:modernlogintute/components/my_textfield.dart';
 import 'package:modernlogintute/components/square_tile.dart';
+import 'package:modernlogintute/services/auth_service.dart';
 
 class LoginPage extends StatefulWidget {
   final Function()? onTap;
@@ -172,16 +173,18 @@ class _LoginPageState extends State<LoginPage> {
                 const SizedBox(height: 50),
 
                 // google + apple sign in buttons
-                const Row(
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     // google button
-                    SquareTile(imagePath: 'lib/images/google.png'),
+                    SquareTile(
+                        onTap: () => AuthService().signInWithGoogle(),
+                        imagePath: 'lib/images/google.png'),
 
-                    SizedBox(width: 25),
+                    const SizedBox(width: 25),
 
                     // apple button
-                    SquareTile(imagePath: 'lib/images/apple.png')
+                    const SquareTile(imagePath: 'lib/images/apple.png')
                   ],
                 ),
 
